418ed142de170d44b8c1e7115dc5eba79fb12ddb
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-01-06 09:54:52 +0300

    Basic Screens and nativewind setup

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View className="bg-slate-500 flex-1 items-center justify-center">
+      <Text className="text-5xl text-blue-500">Feed</Text>
+    </View>
+  )
+}
