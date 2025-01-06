0c125fdfbab77b33a31254a2b9b9da4614bb191f
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-01-06 16:25:32 +0300

    cloudinary connection

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,30 +1,43 @@
-import { Image, Text, View } from 'react-native';
+import { Image, Text, useWindowDimensions, View } from 'react-native';
 import { AntDesign, Feather, Ionicons } from '@expo/vector-icons';
 
+import { Cloudinary } from "@cloudinary/url-gen";
+import { AdvancedImage } from 'cloudinary-react-native';
 
-export default function PostListItem({post}) {
+import { thumbnail } from "@cloudinary/url-gen/actions/resize";
+import { byRadius } from "@cloudinary/url-gen/actions/roundCorners";
+import { focusOn } from "@cloudinary/url-gen/qualifiers/gravity";
+import { FocusOn } from "@cloudinary/url-gen/qualifiers/focusOn";
+import { cld } from '../lib/cloudinary';
+
+
+export default function PostListItem({ post }) {
+    const {width} = useWindowDimensions();
+
+    const image = cld.image(post.image);
+    image.resize(thumbnail().width(width).height(width));
 
     return (
         <View className="bg-white">
-        {/* Header */}
-        <View className="p-3 flex-row items-center gap-2">
-            <Image
-            source={{uri: post.user.image_url}}
-            className="w-12 aspect-square rounded-full"/>
-            <Text className="font-semibold">{post.user.username}</Text>
-        </View>
-    
-        <Image source={{uri: post.image_url}} className="w-full aspect-[4/3]"/>
-    
-        {/* Icons */}
-        <View className="flex-row gap-3 p-3">
-            <AntDesign name="hearto" size={20} />
-            <Ionicons name="chatbubble-outline" size={20} />
-            <Feather name="send" size={20} />
-    
-            <Feather name="bookmark" size={20} className="ml-auto" />
-        </View>
-        
+            {/* Header */}
+            <View className="p-3 flex-row items-center gap-2">
+                <Image
+                    source={{ uri: post.user.image_url }}
+                    className="w-12 aspect-square rounded-full" />
+                <Text className="font-semibold">{post.user.username}</Text>
+            </View>
+
+            <AdvancedImage cldImg={image} className='w-full aspect-[4/3]' />
+
+            {/* Icons */}
+            <View className="flex-row gap-3 p-3">
+                <AntDesign name="hearto" size={20} />
+                <Ionicons name="chatbubble-outline" size={20} />
+                <Feather name="send" size={20} />
+
+                <Feather name="bookmark" size={20} className="ml-auto" />
+            </View>
+
         </View>
     )
 }
