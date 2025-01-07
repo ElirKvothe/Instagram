507f8b1f697abefc509cde9b1b6cf28609a56611
Alperen Yilmaz <114180379+ElirKvothe@users.noreply.github.com>
2025-01-07 10:55:49 +0300

    view posts in database

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,16 +1,37 @@
-import { FlatList } from "react-native";
+import { Alert, FlatList } from "react-native";
 import PostListItem from "~/src/components/PostListItem";
 import posts from "~/assets/data/posts.json";
-
-
+import { useEffect, useState } from "react";
+import { supabase } from "~/src/lib/supabase";
 
 export default function FeedScreen() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    let { data, error } = await supabase.from("posts").select("*, user:profiles(*)");
+    if(error){
+      Alert.alert("Something went wrong");
+    }
+    setPosts(data);
+  };
+
+  console.log(posts)
+
   return (
     <FlatList
       data={posts}
-      renderItem={({item}) => <PostListItem post={item} />}
-      contentContainerStyle={{gap: 10, maxWidth: 512, alignSelf: "center", width: "100%"}}
+      renderItem={({ item }) => <PostListItem post={item} />}
+      contentContainerStyle={{
+        gap: 10,
+        maxWidth: 512,
+        alignSelf: "center",
+        width: "100%",
+      }}
       showsVerticalScrollIndicator={false}
     />
-  )
+  );
 }
